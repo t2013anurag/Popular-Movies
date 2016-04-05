610babd5f9b1031faf09f63616006572c442b13f
t2013anurag <tiwari.anurag126@gmail.com>
2016-04-05 21:27:32 +0530

    Unused classes removed

--- app/src/main/java/com/example/anurag/popular_movies/MainActivity.java
+++ app/src/main/java/com/example/anurag/popular_movies/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.anurag.popular_movies;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
